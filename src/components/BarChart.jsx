74d5fd29ddfc7aa9dfc111153fca319e8723e939
trixtie <jyotish0711@gmail.com>
2023-07-17 12:53:02 +0530

    pull request

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -4,7 +4,7 @@ import { tokens } from "../theme";
 import { mockBarData as data } from "../data/mockData";
 
 const BarChart = ({ isDashboard = false }) => {
-  const theme = useTheme();
+  const theme = useTheme(); 
   const colors = tokens(theme.palette.mode);
 
   return (
